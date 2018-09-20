7af605f9dbb76eda85cac42089be8ba05f131857
Armin <git@armin-warda.de>
2018-09-20 15:15:20 +0200

    changed message

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ class RequestHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        self.wfile.write(b"Hello, world, We are changing!\n")
+        self.wfile.write(b"Hello, bank, We are changing again!\n")
         return
 
 httpd = HTTPServer(('', 8080), RequestHandler)
