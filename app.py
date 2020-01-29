f87b66132f037408388a6d267cfaa1c6ce208e49
Armin <git@armin-warda.de>
2020-01-29 10:59:41 +0100

    typo

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ class RequestHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        self.wfile.write(b"Hello, bank, We are changing again!\n")
+        self.wfile.write(b"Hello, bank, we are changing again!\n")
         return
 
 httpd = HTTPServer(('', 8080), RequestHandler)
