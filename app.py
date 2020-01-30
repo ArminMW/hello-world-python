2a8fd0580e0645dc10ffb8a0d3017c62ae226b21
Armin <git@armin-warda.de>
2020-01-30 14:50:41 +0100

    x

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
+x=0
+
 class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        self.wfile.write(b"Hello, DB/PB, we are changing! Again!\n")
+        x=x+1
+        self.wfile.write(b"Hello, DB/PB, we are changing! Again!\n", x)
         return
 
 httpd = HTTPServer(('', 8080), RequestHandler)
